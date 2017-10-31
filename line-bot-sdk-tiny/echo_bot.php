ceded0b441419151e147e109b9b945ef55b422a1
zenbobot <33239512+zenbobot@users.noreply.github.com>
2017-10-31 09:31:06 +0800

    echo_bot.php

--- line-bot-sdk-tiny/echo_bot.php
+++ line-bot-sdk-tiny/echo_bot.php
@@ -5,7 +5,7 @@
  *
  * LINE Corporation licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
+ * with the License. You may obtain a copy of the License at:FU
  *
  *   https://www.apache.org/licenses/LICENSE-2.0
  *
@@ -18,8 +18,8 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '<your channel access token>';
-$channelSecret = '<your channel secret>';
+$channelAccessToken = 38zJvqJRipecjlyxZ1lZ6ht6SQCjRm+YGnzkU59YDahSGUt+Zc6bw9mgc/b63RNNzfIX10t9Ki7g8Aayl4SNP72WiIsjrruqtfBvaryPAlrey1ff8BNIQuPk2jmmqWXDyvK9wfsX3jTeSqnwRTvmiQdB04t89/1O/w1cDnyilFU=;
+$channelSecret = 28e4ee3fba8bf4e7dafb4781de0e5d0b;
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
